bdfc0350e8fe0c99d54cfc176278b235ff409ac5
Jonathan Lee <jonlee50@hotmail.com>
2017-02-13 12:54:00 -0500

    Downward After Checkout
    Not sure how I did this lmao
    Pure experimental luck

---
+++ Assets/Scripts/CustomerDepart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDepart : MonoBehaviour {
+
+    public GameObject player;
+
+    void OnTriggerEnter(Collider otherGameObject)
+    {
+
+        if (otherGameObject.CompareTag("Player"))
+        {
+            player.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -23,17 +23,33 @@ public class Movement : MonoBehaviour {
 	}
 
 	void OnCollisionEnter (Collision collision) {
+
 		if (collision.gameObject.CompareTag("Counter1")) 
 		{
             force = 0.0f;
             Invoke("Restart", 5.0f);
 		}
-	}
+        if (collision.gameObject.CompareTag("CustomerDepart"))
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+            this.transform.rotation = Quaternion.Euler(1.0f, 0, 0);
+            //rb.AddForce(transform.up * force);
+        }
+
+    }
+
 
     void Restart()
     {
         this.GetComponent<BoxCollider>().enabled = false;
         force = -20.0f;
         rb.AddForce(transform.forward * force);
+        Invoke("ColliderRestart", 2.0f);
     }
+
+    void ColliderRestart()
+    {
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+
 }
