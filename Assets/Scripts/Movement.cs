cfcd8947d91337f3f95e1836bab603a73735de8b
Jeff Li <jeff.li.cf@gmail.com>
2017-02-12 20:04:06 -0500

    imagod

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float force;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		rb.AddForce (transform.forward * force);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.CompareTag("Counter1")) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
